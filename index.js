e5bcc8764124d9c8fe271d79bd33ad58bb0c3528
William-Bulovas <will.bulo@gmail.com>
2018-07-01 17:16:04 -0400

    Fix port

--- index.js
+++ index.js
@@ -29,8 +29,16 @@ function readDataAndCreateJson() {
   });
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/", (req, res) => res.json(borisChenMap));
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   readDataAndCreateJson();
 });
